2b35c687da19d31a15f436879f99a73e0daf633d
Gabriel Sena <gabriel.hsena@outlook.com>
2019-08-15 15:46:10 -0300

    Update WhereMyAnagramsAt.py

--- WhereMyAnagramsAt.py
+++ WhereMyAnagramsAt.py
@@ -18,16 +18,23 @@ anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', '
 anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
 
 '''
-#my solutrion
+# my solution
 
 def anagrams(word, words):
+    # Converting the string into a list, so i can sort it with the sort() method
     word_list = list(word)
+    # Sorting the string
     word_list.sort()
+    # Assign an empty list called result
     result = []
+    
+    # Here is the for so I can iterate in all of the "words" list
+    # So, for each item I'll convert into a list, and them sort it.
+    # if the sorted item is equal the word, then it's an anagrama
     for item in words:
-        item_list = list(item)
-        item_list.sort()
-        if word_list == item_list:
+        item_list = list(item)      # converting the list
+        item_list.sort()            # sorting the word
+        if word_list == item_list:  # if the word is equal to the item, then append to result
             result.append(item)
-    return result
+    return result                   # return result
 
